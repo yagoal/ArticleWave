c47d3f6eee8fa7ff3fc3cc8413ff889a2c340972
yagoal <br.yago@gmail.com>
2024-05-11 13:09:00 -0300

    Initial Commit

---
+++ ArticleWave/ArticleWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleWaveApp.swift
+//  ArticleWave
+//
+//  Created by Yago Pereira on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticleWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
